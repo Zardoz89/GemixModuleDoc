c5c600896e1c1c37df6fe8103905b9982e4150c9
Luis Panadero Guardeño <luis.panadero@gmail.com>
2021-11-04 18:03:30 +0100

    Prototype finds functions and detects overload functions

--- moduleDoc.d
+++ moduleDoc.d
@@ -3,7 +3,13 @@
   name "moduleDoc"
   +/
 import std.stdio;
-import std.getopt;
+import std.regex : split, ctRegex;
+import std.algorithm.searching : findSkip, findSplit, findSplitBefore;
+import std.conv : to;
+import std.range : drop;
+import std.range.primitives : isInputRange;
+
+private enum EOL_REGEX = ctRegex!`[)]|\r\n|\n|$`;
 
 /// Categoria del módulo
 enum Category {
@@ -26,7 +32,8 @@ enum System {
 }
 
 /// Tipos de Gemix
-enum Type {
+enum BasicType {
+  Unknow = "?",
   Byte   = "BYTE",
   Word   = "WORD",
   DWord  = "DWORD",
@@ -35,9 +42,25 @@ enum Type {
   Int16  = "INT16",
   Int32  = "INT32",
   Int64  = "INT64",
+  UInt   = "UINT",
+  UInt8  = "UINT8",
+  UInt16 = "UINT16",
+  UInt32 = "UINT32",
+  UInt64 = "UINT64",
   Float  = "FLOAT",
   Double = "DOUBLE",
   String = "STRING"
+}
+
+struct Type {
+  BasicType type;
+  size_t indirectionLevel = 0;
+  
+  public string toString() {
+    import std.algorithm.iteration : joiner;
+    import std.range : repeat;
+    return type ~ "*".repeat(this.indirectionLevel).joiner().to!string;
+  }
 }
 
 /**
@@ -47,7 +70,7 @@ class ModuleInfo {
   string name;
   Category category = Category.Null;
   System system = System.Null;
-  FunctionInfo[string] functions;
+  FunctionInfo[][string] functions;
 
   override
   public string toString() {
@@ -56,6 +79,7 @@ class ModuleInfo {
       ~ "Category: " ~ this.category ~ ", "
       ~ "System: " ~ this.system;
     if (functions.length > 0) {
+      ret ~= ", ";
       import std.conv : to;
       ret ~= functions.to!string;
     }
@@ -65,6 +89,7 @@ class ModuleInfo {
 
 class FunctionInfo {
   string signature;
+  string functionName;
   Type returnType;
   ParamInfo[] params;
   string docText;
@@ -73,13 +98,14 @@ class FunctionInfo {
   public string toString() {
     import std.conv : to;
     string ret =
-      "Signature: " ~ this.signature ~  ", "
-      ~ "Return: " ~ this.returnType ~ ", "
+      "Name: " ~ this.functionName ~  ", "
+      ~ "Signature: " ~ this.signature ~  ", "
+      ~ "Return: " ~ this.returnType.toString() ~ ", "
       ~ "Params: " ~ this.params.to!string;
     if (docText.length > 0) {
       ret ~= ", " ~ docText;
     }
-    return "f{" ~ ret ~ "}";
+    return "f{" ~ ret ~ "}\n";
   }
 }
 
@@ -91,9 +117,9 @@ class ParamInfo {
 
   override
   public string toString() {
-    string ret = 
+    string ret =
       "Name: " ~ this.name ~  ", "
-      ~ "Type: " ~ this.type;
+      ~ "Type: " ~ this.type.toString();
     if (defaultValue.length > 0) {
       ret ~= "= " ~ defaultValue;
     }
@@ -107,6 +133,7 @@ class ParamInfo {
 enum VERSION = "v0.1.0";
 
 int main(string[] args) {
+  import std.getopt;
   bool showVersion;
   auto helpInformation = getopt(
       args,
@@ -134,7 +161,7 @@ int main(string[] args) {
   }
 
   writeln(moduleInfos);
-  
+
   return 0;
 }
 
@@ -142,164 +169,176 @@ int main(string[] args) {
  * Lee un fichero fuente C o C++ y genera un ModuleInfo con la información parseada del fichero
  */
 ModuleInfo processFile(string fileName) {
-  auto f = File(fileName, "r");
-  scope(exit) {
-    f.close();
-  }
+  import std.file : readText;
+  import std.utf : byUTF;
 
   ModuleInfo moduleInfo = new ModuleInfo();
-  import std.algorithm.iteration : map;
-  import std.algorithm.mutation : stripRight;
-  foreach (line; f.byLineCopy().map!(l => l.stripRight('\r'))) {
-    processHeaderCommentBlock(line, moduleInfo);
-    processLibraryProperties(line, moduleInfo);
-    processLibraryExportsBlock(line, moduleInfo);
-  }
+  auto fileData = readText(fileName).byUTF!char()
+    .processHeaderCommentBlock(moduleInfo)
+    .processLibraryProperties(moduleInfo)
+    .processLibraryExportsBlock(moduleInfo);
 
   return moduleInfo;
 }
 
-private bool headerCommentBlock;
-private bool procesedHeaderCommentBlock;
-private void processHeaderCommentBlock(string line, ModuleInfo moduleInfo) {
-  import std.algorithm.searching : findSkip;
-  import std.regex : split, regex;
-  if (procesedHeaderCommentBlock) {
-    return;
-  }
-
-  if(line.findSkip("/**")) {
-    headerCommentBlock = true;
-  }
-
-  if(headerCommentBlock && line.findSkip("@name")) {
-    import std.algorithm.mutation : strip, stripRight;
-    import std.conv : to;
-    moduleInfo.name = line.strip(' ').stripRight('*');
-  }
-
-  if(headerCommentBlock && line.findSkip("*/")) {
-    headerCommentBlock = false;
-    procesedHeaderCommentBlock = true;
-  }
-}
-
-private bool libraryPropertiesBlock;
-private bool procesedLibraryPropertiesBlock;
-private bool propertyCategoryBlock;
-private bool propertySystemBlock;
-private void processLibraryProperties(string line, ModuleInfo moduleInfo) {
-  import std.algorithm.searching : findSkip;
-  import std.regex : split, regex;
-
-  if (procesedLibraryPropertiesBlock) {
-    return;
-  }
-
-  if (line.findSkip("GMXDEFINE_LIBRARY_PROPERTIES")) {
-    libraryPropertiesBlock = true;
-  }
-
-  if (libraryPropertiesBlock && line.findSkip("GMXDEFINE_PROPERTY_CATEGORY")) {
-    propertyCategoryBlock = true;
-  }
-  if (propertyCategoryBlock) {
-    if (line.findSkip("GMXCATEGORY_")) {
-      auto categoryToken = line.split(regex("[)]|$"));
-      switch (categoryToken[0]) {
-        case "GENERIC":
-          moduleInfo.category = Category.Generic;
-          break;
-
-        case "AUDIO":
-          moduleInfo.category = Category.Audio;
-          break;
-
-        case "GRAPHICS":
-          moduleInfo.category = Category.Graphics;
-          break;
-
-        case "INPUT":
-          moduleInfo.category = Category.Input;
-          break;
-
-        case "PHYSICS":
-          moduleInfo.category = Category.Physics;
-          break;
-
-        case "UNKNOWN":
-        default:
-          moduleInfo.category = Category.Unknown;
-      }
-      propertyCategoryBlock = false;
+private R processHeaderCommentBlock(R)(R text, ModuleInfo moduleInfo)
+if (isInputRange!R)
+{
+  if(text.findSkip("/**")) {
+    if(text.findSkip("@name")) {
+      import std.algorithm.mutation : strip;
+      string nameToken = text.to!string.split(EOL_REGEX)[0];
+      moduleInfo.name = nameToken.strip('\t').strip(' ').strip('*');
+      text = text.drop(nameToken.length);
     }
   }
+  text.findSkip("*/");
+  return text;
+}
 
-  if (libraryPropertiesBlock && line.findSkip("GMXDEFINE_PROPERTY_SYSTEM")) {
-    propertySystemBlock = true;
-  }
-  if (propertySystemBlock) {
-    if (line.findSkip("GMXSYSTEM_")) {
-      auto systemToken = line.split(regex("[)]|$"));
-      switch (systemToken[0]) {
-        case "COMMON":
-          moduleInfo.system = System.Common;
-          break;
+private R processLibraryProperties(R)(R text, ModuleInfo moduleInfo)
+if (isInputRange!R)
+{
+  if (text.findSkip("GMXDEFINE_LIBRARY_PROPERTIES")) {
+    if (text.findSkip("GMXDEFINE_PROPERTY_CATEGORY")) {
+      if (text.findSkip("GMXCATEGORY_")) {
+        auto categoryToken = text.to!string.split(EOL_REGEX);
+        switch (categoryToken[0]) {
+          case "GENERIC":
+            moduleInfo.category = Category.Generic;
+            break;
 
-        case "LEGACY":
-          moduleInfo.system = System.Legacy;
-          break;
+          case "AUDIO":
+            moduleInfo.category = Category.Audio;
+            break;
 
-        case "MODERN":
-          moduleInfo.system = System.Modern;
-          break;
+          case "GRAPHICS":
+            moduleInfo.category = Category.Graphics;
+            break;
 
-        case "UNKNOWN":
-        default:
-          moduleInfo.system = System.Unknown;
+          case "INPUT":
+            moduleInfo.category = Category.Input;
+            break;
+
+          case "PHYSICS":
+            moduleInfo.category = Category.Physics;
+            break;
+
+          case "UNKNOWN":
+          default:
+            moduleInfo.category = Category.Unknown;
+        }
+        text = text.drop(categoryToken[0].length);
       }
-      propertySystemBlock = false;
+    }
+
+    if (text.findSkip("GMXDEFINE_PROPERTY_SYSTEM")) {
+      if (text.findSkip("GMXSYSTEM_")) {
+        auto systemToken = text.to!string.split(EOL_REGEX);
+        switch (systemToken[0]) {
+          case "COMMON":
+            moduleInfo.system = System.Common;
+            break;
+
+          case "LEGACY":
+            moduleInfo.system = System.Legacy;
+            break;
+
+          case "MODERN":
+            moduleInfo.system = System.Modern;
+            break;
+
+          case "UNKNOWN":
+          default:
+            moduleInfo.system = System.Unknown;
+        }
+        text = text.drop(systemToken[0].length);
+      }
     }
   }
-
-  if (libraryPropertiesBlock && moduleInfo.system != System.Null && moduleInfo.category != Category.Null) {
-    procesedLibraryPropertiesBlock = true;
-    libraryPropertiesBlock = false;
-  }
+  return text;
 }
 
-private bool libraryExportsBlock;
-private bool procesedLibraryExportsBlock;
-private void processLibraryExportsBlock(string line, ModuleInfo moduleInfo) {
-  import std.algorithm.searching : findSkip;
-  import std.regex : split, regex;
-
-  if (procesedLibraryExportsBlock) {
-    return;
+private R processLibraryExportsBlock(R)(R text, ModuleInfo moduleInfo)
+if (isInputRange!R)
+{
+  if (text.findSkip("GMXDEFINE_LIBRARY_EXPORTS")) {
+    text = text.processFunctionsBlock(moduleInfo);
   }
-
-  if (line.findSkip("GMXDEFINE_LIBRARY_EXPORTS")) {
-    libraryExportsBlock = true;
-  }
-
-  processFunctionsBlock(line, moduleInfo);
+  return text;
 }
 
-private bool functionsBlock;
-private bool procesedFunctionsBlock;
-private void processFunctionsBlock(string line, ModuleInfo moduleInfo) {
-  import std.algorithm.searching : findSkip, findSplit;
-  import std.regex : split, regex;
+private R processFunctionsBlock(R)(R text, ModuleInfo moduleInfo)
+if (isInputRange!R)
+{
+  import std.algorithm.mutation : stripLeft, stripRight;
+  import std.range : chunks, stride;
+  import std.regex : splitter;
+  if (text.findSkip("GMXDEFINE_FUNCTIONS(")) {
+    text = text.stripLeft('\r').stripLeft('\n').stripLeft(' ').stripLeft('\t').stripLeft(' ');
+    // TODO
+    string functionsBlock = text.findSplitBefore(");")[0].to!string;
+    auto fTokens = functionsBlock.splitter(ctRegex!`,\s+`).chunks(2).stride(2);
+    // Cada entrada en fTokens, es una definición de una funcion : "sigantura", "retorno"
+    foreach(fToken; fTokens) {
+      auto functionInfo = processFunctionToken(fToken); 
+      moduleInfo.functions[functionInfo.functionName] ~= functionInfo;
+    }
 
-  if (procesedFunctionsBlock) {
-    return;
+    text = text.drop(functionsBlock.length);
   }
-
-  if (line.findSkip("GMXDEFINE_FUNCTIONS")) {
-    functionsBlock = true;
-  }
-
-  // todo
-
+  return text;
 }
 
+private FunctionInfo processFunctionToken(R)(R fToken)
+if (isInputRange!R)
+{
+  import std.regex : matchFirst;
+  import std.string : strip;
+
+  FunctionInfo funcionInfo = new FunctionInfo();
+  funcionInfo.signature = fToken.front.strip("\"");
+  funcionInfo.functionName = funcionInfo.signature.matchFirst(ctRegex!`[a-zA-Z0-9_-]+`).hit;
+  fToken.popFront();
+  funcionInfo.returnType = fToken.front.getTypeFromText;
+  return funcionInfo;
+}
+
+private Type getTypeFromText(R)(R text)
+if (isInputRange!R)
+{
+  import std.algorithm.searching : canFind, count;
+
+  Type type;
+  if (text.canFind("I64")) {
+    type.type = BasicType.Int64;
+  } else if (text.canFind("UI64")) {
+    type.type = BasicType.UInt64;
+  } else if (text.canFind("UI32")) {
+    type.type = BasicType.UInt32;
+  } else if (text.canFind("UI16")) {
+    type.type = BasicType.UInt16;
+  } else if (text.canFind("UI8")) {
+    type.type = BasicType.UInt8;
+  } else if (text.canFind("I64")) {
+    type.type = BasicType.Int64;
+  } else if (text.canFind("I32")) {
+    type.type = BasicType.Int32;
+  } else if (text.canFind("I16")) {
+    type.type = BasicType.Int16;
+  } else if (text.canFind("I8")) {
+    type.type = BasicType.Int8;
+  } else if (text.canFind("UI")) {
+    type.type = BasicType.UInt;
+  } else if (text.canFind("I")) {
+    type.type = BasicType.Int;
+  } else if (text.canFind("F")) {
+    type.type = BasicType.Float;
+  } else if (text.canFind("D")) {
+    type.type = BasicType.Double;
+  } else if (text.canFind("S")) {
+    type.type = BasicType.String;
+  }
+  type.indirectionLevel = text.count('P');
+  return type;
+}
